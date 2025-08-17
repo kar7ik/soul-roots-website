95edab3339eee0f3bbd0030f4d38c1bfb0e36fba
Kar7ik <43144440+kar7ik@users.noreply.github.com>
2025-08-18 01:07:39 +1000

    changing color

---
+++ assets/js/tailwind-config.js
@@ -0,0 +1,15 @@
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        forest: "#355E3B",
+        terracotta: "#A0522D",
+        sand: "#EADBC8",
+        sage: "#8F9779",
+        ochre: "#C99700",
+        charcoal: "#3B2F2F",
+      }
+    }
+  }
+}
